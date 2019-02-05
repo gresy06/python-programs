8ea74883632d09a917d0c1ca36d09f91a8f2c4bf
gresy06 <37238681+gresy06@users.noreply.github.com>
2019-02-05 11:13:14 +0530

    Create largestnumber.py

---
+++ largestnumber.py
@@ -0,0 +1,11 @@
+num1 = 78
+num2 = 89
+num3 = 14
+if (num1 >= num2) and (num1 >= num3):
+   largest = num1
+if (num2 >= num1) and (num2 >= num3):
+   largest = num2
+print("the largest number between",num1,",",num2,"and",num3,"is",largest)
+   
+
+
