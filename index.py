138147973d754141f3083244a37d6710f0df9a73
gresy06 <37238681+gresy06@users.noreply.github.com>
2019-08-08 12:37:57 +0530

    Update index.py

--- index.py
+++ index.py
@@ -2,12 +2,9 @@ def reorder(arr,index, n):
   
     temp = [0] * n; 
   
-    # arr[i] should be 
-        # present at index[i] index 
     for i in range(0,n): 
         temp[index[i]] = arr[i] 
-  
-    # Copy temp[] to arr[] 
+ 
     for i in range(0,n): 
         arr[i] = temp[i] 
         index[i] = i 
